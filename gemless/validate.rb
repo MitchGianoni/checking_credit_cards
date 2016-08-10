e18edbcabed7cf2fcaa0e836f588a649667cd2bd
MitchGianoni <mitchgianoni@yahoo.com>
2016-08-10 03:27:32 -0400

    refactor

--- gemless/validate.rb
+++ gemless/validate.rb
@@ -3,8 +3,8 @@ require 'pry'
 array = ["4111111111111111","4111111111111","4012888888881881","378282246310005",
 	"6011111111111117","5105105105105100","5105 1051 0510 5106","9111111111111111"]
 
-def credit_card_valid?(account_number)
-  digits = account_number.chars.map(&:to_i)
+def valid?(account)
+  digits = account.chars.map(&:to_i)
   check = digits.pop
 
   sum = digits.reverse.each_slice(2).flat_map do |x, y|
@@ -16,18 +16,8 @@ def credit_card_valid?(account_number)
 
 end
 
-validity1 = credit_card_valid?(array[0])
-validity2 = credit_card_valid?(array[1])
-validity3 = credit_card_valid?(array[2])
-validity4 = credit_card_valid?(array[3])
+cards = ["#{valid?(array[0])},#{valid?(array[1])},#{valid?(array[2])},#{valid?(array[3])},#{valid?(array[4])},#{valid?(array[5])},#{valid?(array[6])},#{valid?(array[7])}"]
 
-validity5 = credit_card_valid?(array[4])
-validity6 = credit_card_valid?(array[5])
-validity7 = credit_card_valid?(array[6])
-validity8 = credit_card_valid?(array[7])
-
-cards = "#{validity1}, #{validity2}, #{validity3}, #{validity4}, #{validity5}, #{validity6}, #{validity7}, #{validity8}"
-
-binding.pry
+#binding.pry
 
 puts cards
